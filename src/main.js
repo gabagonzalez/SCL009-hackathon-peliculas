e549287017e26cff414089c17255903f5cdf6520
donapaz <danielapaz.pl@gmail.com>
2019-05-22 11:11:22 -0400

    Muestra en html todos los resultados por título

--- src/main.js
+++ src/main.js
@@ -3,25 +3,40 @@ const dataOMDB ="http://www.omdbapi.com/?i=tt3896198&apikey=5a2182d7";
 
 
 const btnSearchName = document.getElementsByTagName("button")[0];
-const result= document.getElementById("root");
+const result= document.getElementById("show-data");
 
 
 //OBTENER PELÏCULAS POR NOMBRE
 btnSearchName.addEventListener('click',(e) =>{
   e.preventDefault();
+  result.innerHTML = "";
   let flickName = document.getElementsByTagName("input")[0].value;
   showResult(dataOMDB)
   
   function showResult(dataOMBD) {
 
     let searchTitle = flickName;
-    fetch(dataOMBD + "&t=" + searchTitle)
+    fetch(dataOMBD + "&s=" + searchTitle)
         .then(function(response){
           return response.json();
         })
   
-        .then (function (element) {
-            result.innerHTML += `<img src="${element.Poster}" alt="${element.Title}">`
+        .then (function (data) {
+          const searchByTitle = data.Search;
+          
+            searchByTitle.forEach(element => {
+              result.innerHTML +=
+              `<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-2" >
+              <div class="card border-dark text-center rounded-lg mb-3"  >
+                <img src="${element.Poster}" class="card-img-top" alt="${element.Title}">
+                
+      
+                <button type="button" id="info" class="btn btn-primary" data-toggle="modal" data-target="#modal${element.imbdID}">Ver más</button>
+              </div>
+            </div>`
+            })
+        
+
         })
      
   }
