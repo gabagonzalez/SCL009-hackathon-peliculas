294a1dccecb7a8e93648800583c2251356caf582
gabagonzalez <gaba2801@gmail.com>
2019-05-23 10:24:26 -0400

    index2 y super2.js

--- src2/super2.js
+++ src2/super2.js
@@ -1,7 +1,7 @@
 /* Manejo de data */
 // Mi Data, parametro con el que traigo la info
 
-let arrSuper = ["Spider-man","Avengers: Endgame","Spider-Man: Far from Home ","Shazam!","Aquaman","Captain Marvel", "Justice League vs the Fatal Five", "Reign of the Supermen", "Batman Robin", "Batman"];
+let arrSuper = ["Batman: The Animated"];
 const container = document.getElementById("movie"); //para visualizar en container
 let randomNumb = Math.floor((Math.random() * arrSuper.length - 1) + 1); //numero aleatorio segun elementos del array
 let randomMov = arrSuper[randomNumb]; //el número representa una pelicula
@@ -28,18 +28,7 @@ function getMovie() {
                     <a href="super2.html" title="Información peliculas" class="navbar-brand">Siguiente</a>
 	  	           </div>
 	  	        </div>
-	  	        <div class="row">
-          <div class="well">
-          <ul class="list-group">
-            <h4><li class="list-group-item"><h2>Genero:</h2> ${movie.Genre}</li>
-            <li class="list-group-item"><h2>Estreno:</h2> ${movie.Released}</li>
-            <li class="list-group-item"><h2>Clasificación:</h2> ${movie.Rated}</li>
-            <li class="list-group-item"><h2>Rating:</h2> ${movie.imdbRating}</li>
-            <li class="list-group-item"><h2>Actores:</h2> ${movie.Actors}</li>
-            <li class="list-group-item"><h2>Languaje:</h2> ${movie.Language}</li>
-          </ul> 
-					</div>
-	  	        </div>
+	  	        
 	  	      `;
       $('#movie').html(output);
     })
